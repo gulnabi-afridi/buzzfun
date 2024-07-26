f67a1e9ea0d063d710ba2185495e66756a5fe066
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2024-07-26 19:06:43 +0500

    edit modal developed

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -10,11 +10,14 @@ import Following from "../components/Profile/Following";
 import Replies from "../components/Profile/Replies";
 import CoinsLaunched from "../components/Profile/CoinsLaunched";
 import CoinsHeld from "../components/Profile/CoinsHeld";
+import { useDisclosure } from "@mantine/hooks";
+import { Modal, Button } from "@mantine/core";
 
 const ProfilePage = () => {
   // states ----------->
   const [selectedIndex, setSelectedIndex] = useState(null);
   const [selectedName, setSelectedName] = useState("");
+  const [opened, { open, close }] = useDisclosure(false);
 
   //   functions --------->
   const handleButtonClick = (index: number, name: string) => {
@@ -23,10 +26,11 @@ const ProfilePage = () => {
   };
 
   return (
-    <ComponentWrapper style="bg-white-2">
-      <div className="w-full pt-12 min-h-screen pb-10 flex justify-center items-start">
-        <div
-          className={`w-full grid gap-6 justify-center items-start 
+    <React.Fragment>
+      <ComponentWrapper style="bg-white-2">
+        <div className="w-full pt-12 min-h-screen pb-10 flex justify-center items-start">
+          <div
+            className={`w-full grid gap-6 justify-center items-start 
             ${
               selectedName === "Replies" &&
               "max-w-[950px] grid-cols-[1fr,1fr,2fr]"
@@ -40,125 +44,209 @@ const ProfilePage = () => {
               "max-w-[860px] grid-cols-[1fr,1fr,1.5fr]"
             }
           `}
-        >
-          {/* uer profile ----> */}
-          <div className="w-full flex flex-col gap-3">
-            <div className="w-full flex flex-col border-[1px] border-blue-1 rounded-[2px] py-[8px] px-[7px]">
-              {/* profile image */}
-              <div className="w-full h-[200px] relative">
-                <Image src="/assets/profile.png" alt="" fill />
+          >
+            {/* uer profile ----> */}
+            <div className="w-full flex flex-col gap-3">
+              <div className="w-full flex flex-col border-[1px] border-blue-1 rounded-[2px] py-[8px] px-[7px]">
+                {/* profile image */}
+                <div className="w-full h-[200px] relative">
+                  <Image src="/assets/profile.png" alt="" fill />
+                </div>
+                {/* user name */}
+                <div className="w-full mt-3 border-[1px] border-blue-1 grid grid-cols-[1fr,1.4fr] px-[5px] h-[43px] justify-center items-center">
+                  <p className="text-[14px] uppercase font-normal text-black-1">
+                    Username:
+                  </p>
+                  <input
+                    placeholder="insert username"
+                    type="text"
+                    className="w-full focus:outline-none px-1 text-[10px] font-normal placeholder:text-white-1 text-white-1 uppercase h-[60%] bg-[#404040]"
+                    name="name"
+                    id="name"
+                  />
+                </div>
+                {/* edit your profile */}
+                <div className="flex justify-start mt-5 mb-3 items-center gap-2 pl-4">
+                  <button
+                    onClick={open}
+                    className="text-white-1 hover:opacity-80 font-normal text-[10px] uppercase bg-black-1 px-2 py-[2px]"
+                  >
+                    Edit
+                  </button>
+                  <p className="text-[10px] uppercase font-normal text-black-1">
+                    Your Profile
+                  </p>
+                </div>
               </div>
-              {/* user name */}
-              <div className="w-full mt-3 border-[1px] border-blue-1 grid grid-cols-[1fr,1.4fr] px-[5px] h-[43px] justify-center items-center">
-                <p className="text-[14px] uppercase font-normal text-black-1">
-                  Username:
+              {/* referal link ---> */}
+              <div className="w-full h-[44px] bg-transparent border-[1px] border-blue-1 rounded-[2px] flex justify-between items-center px-2 gap-3">
+                <p className="text-[10px]  uppercase font-normal text-black-1">
+                  Referral Link
                 </p>
-                <input
-                  placeholder="insert username"
-                  type="text"
-                  className="w-full focus:outline-none px-1 text-[10px] font-normal placeholder:text-white-1 text-white-1 uppercase h-[60%] bg-[#404040]"
-                  name="name"
-                  id="name"
-                />
-              </div>
-              {/* edit your profile */}
-              <div className="flex justify-start mt-5 mb-3 items-center gap-2 pl-4">
-                <button className="text-white-1 hover:opacity-80 font-normal text-[10px] uppercase bg-black-1 px-2 py-[2px]">
-                  Edit
+                <button className="px-2 py-1 text-[10px] font-normal text-white-1 bg-[#404040]">
+                  Generate
                 </button>
-                <p className="text-[10px] uppercase font-normal text-black-1">
-                  Your Profile
-                </p>
+                <button className="w-[18px] h-[18px] bg-white-1 flex justify-center items-center">
+                  <FiCopy className="text-black-1 text-[12px]" />
+                </button>
+              </div>
+              {/* reactions to profile */}
+              <div className="bg-white-1 w-fit py-2 px-4 flex justify-center items-center gap-3">
+                <div className="flex justify-center items-center gap-1">
+                  <p>❤️</p>
+                  <p className="text-[10px] font-normal text-black-1">12</p>
+                </div>
+                <div className="flex justify-center items-center gap-1">
+                  <p>🤬</p>
+                  <p className="text-[10px] font-normal text-black-1">12</p>
+                </div>
               </div>
             </div>
-            {/* referal link ---> */}
-            <div className="w-full h-[44px] bg-transparent border-[1px] border-blue-1 rounded-[2px] flex justify-between items-center px-2 gap-3">
-              <p className="text-[10px]  uppercase font-normal text-black-1">
-                Referral Link
+            {/* Bio ---> */}
+            <div className="w-full mt-5 flex flex-col">
+              <p className="text-[10px] uppercase font-normal text-black-1">
+                Bio:
               </p>
-              <button className="px-2 py-1 text-[10px] font-normal text-white-1 bg-[#404040]">
-                Generate
-              </button>
-              <button className="w-[18px] h-[18px] bg-white-1 flex justify-center items-center">
-                <FiCopy className="text-black-1 text-[12px]" />
-              </button>
-            </div>
-          </div>
-          {/* Bio ---> */}
-          <div className="w-full mt-5 flex flex-col">
-            <p className="text-[10px] uppercase font-normal text-black-1">
-              Bio:
-            </p>
-            <p className="border-[1px] border-blue-1 text-[10px] font-normal text-black-1 px-2 py-1">
-              bio goes here. make sure it is interesting otherwise people will
-              hate you.
-            </p>
-            <div className="mt-10 flex flex-col px-2 py-1 border-[1px] border-blue-1">
-              {/* header */}
-              <div className="w-full h-[30px] justify-center items-center grid grid-cols-[1.5fr,1fr,1fr]">
-                <h2 className="text-[15px] font-normal uppercase text-black-1">
-                  BUZZ XP
-                </h2>
-                <h2 className="text-[15px] font-normal uppercase text-black-1">
-                  4000{" "}
-                </h2>
-              </div>
-              {/* rows */}
-              <div className="flex flex-col">
-                {userDetails.map((item, index) => {
-                  // Determine if the current item is selected
-                  const isSelected = selectedIndex === index;
+              <p className="border-[1px] border-blue-1 text-[10px] font-normal text-black-1 px-2 py-1">
+                bio goes here. make sure it is interesting otherwise people will
+                hate you.
+              </p>
+              <div className="mt-10 flex flex-col px-2 py-1 border-[1px] border-blue-1">
+                {/* header */}
+                <div className="w-full h-[30px] justify-center items-center grid grid-cols-[1.5fr,1fr,1fr]">
+                  <h2 className="text-[15px] font-normal uppercase text-black-1">
+                    BUZZ XP
+                  </h2>
+                  <h2 className="text-[15px] font-normal uppercase text-black-1">
+                    4000{" "}
+                  </h2>
+                </div>
+                {/* rows */}
+                <div className="flex flex-col">
+                  {userDetails.map((item, index) => {
+                    // Determine if the current item is selected
+                    const isSelected = selectedIndex === index;
 
-                  return (
-                    <button
-                      key={index}
-                      onClick={() => handleButtonClick(index, item.name)}
-                      className={`w-full justify-center items-center h-[28px] grid grid-cols-[1.5fr,1fr,1fr] ${
-                        isSelected ? "bg-black text-white" : "bg-transparent"
-                      }`}
-                    >
-                      <p
-                        className={`w-full flex text-left justify-start items-center text-[10px] font-normal ${
-                          isSelected ? "text-white" : "text-black-1"
+                    return (
+                      <button
+                        key={index}
+                        onClick={() => handleButtonClick(index, item.name)}
+                        className={`w-full justify-center items-center h-[28px] grid grid-cols-[1.5fr,1fr,1fr] ${
+                          isSelected ? "bg-black text-white" : "bg-transparent"
                         }`}
                       >
-                        {item.name}
-                      </p>
-                      <p
-                        className={`w-full flex justify-start items-center text-[10px] font-normal ${
-                          isSelected ? "text-white" : "text-black-1"
-                        }`}
-                      >
-                        {item.value}
-                      </p>
-                      <div className="w-full flex justify-end items-center">
-                        <div
-                          className={`w-[12px] flex justify-center items-center h-[12px] ${
-                            isSelected ? "bg-black-1" : "bg-white-1"
+                        <p
+                          className={`w-full flex text-left justify-start items-center text-[10px] font-normal ${
+                            isSelected ? "text-white" : "text-black-1"
                           }`}
                         >
-                          <GoArrowUpRight
-                            className={`text-[11px] ${
-                              isSelected ? "text-white-1" : "text-black-1"
+                          {item.name}
+                        </p>
+                        <p
+                          className={`w-full flex justify-start items-center text-[10px] font-normal ${
+                            isSelected ? "text-white" : "text-black-1"
+                          }`}
+                        >
+                          {item.value}
+                        </p>
+                        <div className="w-full flex justify-end items-center">
+                          <div
+                            className={`w-[12px] flex justify-center items-center h-[12px] ${
+                              isSelected ? "bg-black-1" : "bg-white-1"
                             }`}
-                          />
+                          >
+                            <GoArrowUpRight
+                              className={`text-[11px] ${
+                                isSelected ? "text-white-1" : "text-black-1"
+                              }`}
+                            />
+                          </div>
                         </div>
-                      </div>
-                    </button>
-                  );
-                })}
+                      </button>
+                    );
+                  })}
+                </div>
               </div>
             </div>
+            {/* user - details components  */}
+            {selectedName === "Followers" && <Followers />}
+            {selectedName === "Following" && <Following />}
+            {selectedName === "Replies" && <Replies />}
+            {selectedName === "Coins Launched" && <CoinsLaunched />}
+            {selectedName === "Coins held" && <CoinsHeld />}
           </div>
-          {/* user - details components  */}
-          {selectedName === "Followers" && <Followers />}
-          {selectedName === "Following" && <Following />}
-          {selectedName === "Replies" && <Replies />}
-          {selectedName === "Coins Launched" && <CoinsLaunched />}
-          {selectedName === "Coins held" && <CoinsHeld />}
         </div>
-      </div>
-    </ComponentWrapper>
+      </ComponentWrapper>
+      <Modal
+        opened={opened}
+        onClose={close}
+        centered
+        size='xs'
+        classNames={{
+            
+          body: "bg-white-2 border-[1px] rounded-[2px] border-blue-1",
+        }}
+        withCloseButton={false}
+      >
+        <div className="w-full flex justify-center items-center">
+          <div className="w-full py-6 max-w-[220px] flex flex-col">
+            <div className="flex justify-start mt-5 mb-3 items-center gap-2 pl-4">
+              <button className="text-white-1 hover:opacity-80 font-normal text-[10px] uppercase bg-black-1 px-2 py-[2px]">
+                Edit
+              </button>
+              <p className="text-[10px] uppercase font-normal text-black-1">
+                Your Profile
+              </p>
+            </div>
+            {/* user pic  */}
+            <div className="w-full mt-4 border-[1px] border-blue-1 rounded-[2px] p-2 grid grid-cols-[71px,1fr] justify-center items-center gap-2">
+              <div className="w-full h-[72px] relative">
+                <Image
+                  src="/assets/profile.png"
+                  alt=""
+                  fill
+                  className="object-fill"
+                />
+              </div>
+              <div className="flex justify-center items-center w-full flex-col gap-2">
+                <p className="text-[10px] font-normal text-black-1 uppercase">
+                  Profile photo
+                </p>
+                <button className="text-[10px] font-normal text-black-1 underline">
+                  Edit
+                </button>
+              </div>
+            </div>
+            {/* user name */}
+            <div className="w-full mt-3 border-[1px] border-blue-1 grid grid-cols-[1fr,1.4fr] px-[5px] h-[43px] justify-center items-center">
+              <p className="text-[14px] uppercase font-normal text-black-1">
+                Username:
+              </p>
+              <input
+                placeholder="insert username"
+                type="text"
+                className="w-full focus:outline-none px-1 text-[10px] font-normal placeholder:text-white-1 text-white-1 uppercase h-[60%] bg-[#404040]"
+                name="name"
+                id="name"
+              />
+            </div>
+            <p className="text-[8px] font-normal text-[#343434] my-3">
+              [you can only edit your username <br /> every 69 hours]
+            </p>
+            {/* Bio */}
+            <div className="flex flex-col gap-1">
+              <p className="text-[10px] uppercase font-normal text-black-1">
+                Bio:
+              </p>
+              <p className="border-[1px] border-blue-1 text-[10px] font-normal text-black-1 px-2 py-1">
+                bio goes here. make sure it is interesting otherwise people will
+                hate you.
+              </p>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </React.Fragment>
   );
 };
 
