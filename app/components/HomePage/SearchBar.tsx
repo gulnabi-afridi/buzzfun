35cf109a2065f9e8c83b6518e752da7935dbe943
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2024-07-29 17:35:49 +0500

    search bar updated

--- app/components/HomePage/SearchBar.tsx
+++ app/components/HomePage/SearchBar.tsx
@@ -1,40 +1,75 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
+import { Select } from "@mantine/core";
 
 const SearchBar = () => {
+  const [filter, setFilter] = useState("trending");
+
   return (
-    <div className="flex my-4 justify-between items-center w-full py-2 bg-blue-1 px-3">
+    <div className="flex my-4 justify-between items-center w-full py-2 bg-transparent px-3">
       {/* bread crumbs  */}
-      <div className="flex flex-wrap justify-center items-center gap-1 md:gap-3">
-        {breadCrumbs.map((item, index) => {
-          return (
-            <div key={index} className="flex justify-center items-center gap-4">
-              <p
-                className={`text-base ${
-                  item.isBold ? "font-semibold" : "font-normal"
-                } text-white-1`}
-              >
-                {item.value}{" "}
-              </p>
-              <div
-                className={`w-[2px] ${
-                  index + 1 === breadCrumbs.length ? "hidden" : "block"
-                } h-[20px] bg-white-1 rotate-[20deg]`}
-              ></div>
-            </div>
-          );
-        })}
+      <div className="flex justify-center items-center gap-2">
+        <p className="text-[12px] font-normal mr-3 text-black-1">
+          Select here &gt; &gt;
+        </p>
+        <button
+          onClick={() => setFilter("trending")}
+          className={`text-[16px] ${
+            filter === "trending"
+              ? "bg-purple-1 textShadow text-white-1"
+              : "text-white-1 bg-black-1/55"
+          } font-normal rounded-[2px] px-2 py-[2px]`}
+        >
+          Trending{" "}
+        </button>
+        <button
+          onClick={() => setFilter("new")}
+          className={`text-[16px] ${
+            filter === "new"
+              ? "bg-purple-1  text-white-1 textShadow"
+              : "text-white-1 bg-black-1/55"
+          } font-normal rounded-[2px] px-2 py-[2px]`}
+        >
+          New{" "}
+        </button>
+        <button
+          onClick={() => setFilter("live activity")}
+          className={`text-[16px] ${
+            filter === "live activity"
+              ? "bg-purple-1 textShadow  text-white-1"
+              : "text-white-1 bg-black-1/55"
+          } font-normal rounded-[2px] px-2 py-[2px]`}
+        >
+          Live Activity{" "}
+        </button>
       </div>
       {/* search input */}
-      <div className="w-[340px] px-2 h-[28px] bg-white-1 rounded-md grid grid-cols-[1fr,20px]">
-        <input
-          type="text"
-          className="bg-transparent w-full focus:outline-none h-full placeholder:text-black-1 text-[10px] font-normal"
-          placeholder="Search token by name or ticker"
+      <div className="flex justify-center items-center gap-4">
+        <Select
+          size="xs"
+          classNames={{
+            root: "flex justify-center items-center gap-2",
+            label: "text-[16px] font-normal text-black-1",
+            input:
+              "text-[14px] !w-[120px] !rounded-[2px] focus:border-white-1 font-normal text-black-1",
+          }}
+          label="Network"
+          placeholder="Pick value"
+          data={["All", "None", "Single", "Multiple"]}
         />
-        <div className="w-full flex justify-center items-center">
-          <Image src="/assets/enter.svg" alt="" width={21} height={18} />
+        <div className="w-[340px] px-2 h-[28px] bg-white-1 rounded-[2px] grid grid-cols-[1fr,20px]">
+          <input
+            type="text"
+            className="bg-transparent w-full focus:outline-none h-full placeholder:text-black-1 text-[10px] font-normal"
+            placeholder="Search token by name or ticker"
+          />
+          <div className="w-full flex justify-center items-center">
+            <Image src="/assets/enter.svg" alt="" width={21} height={18} />
+          </div>
         </div>
+        <p className="text-[12px] font-normal text-black-1">{pattern1}</p>
       </div>
     </div>
   );
@@ -42,23 +77,4 @@ const SearchBar = () => {
 
 export default SearchBar;
 
-
-const breadCrumbs = [
-    {
-      isBold: true,
-      value: "Trending ",
-    },
-    {
-      isBold: false,
-      value: "New ",
-    },
-    {
-      isBold: false,
-      value: "Live Activity ",
-    },
-    {
-      isBold: false,
-      value: "Hall of shame ",
-    },
-  ];
-  
+const pattern1 = "<< Search here";
