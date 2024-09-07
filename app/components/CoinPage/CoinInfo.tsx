685ab82b786b0e370a90f7661b6dfc07b16ea1f0
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2024-09-07 14:57:53 +0500

    project des @com done

--- app/components/CoinPage/CoinInfo.tsx
+++ app/components/CoinPage/CoinInfo.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import StatsHeader from './StatsHeader'
+import SocialLinks from './SocialLinks'
+import TaxMaxBuy from './TaxMaxBuy'
+import ProjectDes from './ProjectDes'
 
 const CoinInfo:React.FC = () => {
   return (
     <div className='w-full flex flex-col'>
         <StatsHeader/>
+        <SocialLinks/>
+        <TaxMaxBuy/>
+        <ProjectDes/>
     </div>
   )
 }
